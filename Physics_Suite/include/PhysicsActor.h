c71aa27d44f054f40dd8999f43a24232fc48695f
Dylan Ascencio <dylanascen@gmail.com>
2018-04-02 14:21:59 -0500

    Simplified PhysicsActor to include just position, angle, mass, velocity, and angular velocity. PhysicsActor will no longer store forces to apply when Update(float) is called; instead, forces and other changes to properties will be applied as they occur via ApplyForce(), ApplyImpulse(), etc.

--- Physics_Suite/include/PhysicsActor.h
+++ Physics_Suite/include/PhysicsActor.h
@@ -4,11 +4,16 @@
 class PhysicsActor {
 private:
 	vec2f _position;
-	vec2f _velocity;
-	vec2f _momentum;
-	vec2f _force;
+	float _angle;
 	float _mass;
+	
+	vec2f _velocity;
+	float _angular_velocity;
 public:
 	void Update(float);
+	void ApplyForce(vec2f, float);	
+	void ApplyAcceleration(Vec2f, float);
+	void ApplyImpulse(vec2f);
+	void ApplyVelocity(vec2f);
 	virtual void Render() = 0;
 };
--- Physics_Suite/source/PhysicsActor.cpp
+++ Physics_Suite/source/PhysicsActor.cpp
@@ -1,7 +1,21 @@
 #include "..\include\PhysicsActor.h"
 
-void PhysicsActor::Update(float dt) {
-	_momentum += _force * dt;
-	_velocity = _momentum / mass;                                            
+void PhysicsActor::Update(float dt) {                                           
 	_position += _velocity * dt;
+}
+
+void PhysicsActor::ApplyForce(vec2f force, float dt) {
+	_velocity += (force * dt) / _mass;
+}
+
+void PhysicsActor::ApplyAcceleration(vec2f acceleration, dt) {
+	_velocity += (acceleration * dt);
+}
+
+void PhysicsActor::ApplyImpulse(vec2f impulse) {
+	_velocity += impulse / _mass;
+}
+
+void PhysicsActor::ApplyVelocity(vec2f velocity_change) {
+	_velocity += velocity_change;
 }
